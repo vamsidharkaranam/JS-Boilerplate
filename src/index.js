da9ce0edd08befe49bb0c8979bcd44d74b3d9945
Vamsidhar Karanam <vamsidhar.karanam@gmail.com>
2019-09-25 13:51:26 -0400

    added validator and uuid

--- src/index.js
+++ src/index.js
@@ -1,7 +1,12 @@
 import double, { add, name } from './utilities'
 import scream from './scream'
+import validator from 'validator'
+import uuidv4 from 'uuid/v4'
 
 console.log('index.js')
 console.log(add(32, 3))
 console.log(scream(name))
 console.log(double(3))
+const testEmail = validator.isEmail('vamsidharKaranam')
+console.log(testEmail)
+console.log(uuidv4())
